3405007f4f39904cc9f99af489f1946ac2185846
krithick14 <56062143+krithick14@users.noreply.github.com>
2020-10-01 19:28:23 +0530

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,9 @@
+sudo make 
+sudo make install 
+sudo apt-get install toilet
+sudo apt-get install tk
+sudo apt-get install xterm
+sudo apt-get update
+clear
+autoprox &
+
---
+++ main.py
@@ -0,0 +1,81 @@
+import tkinter as tk
+from tkinter import *
+from tkinter import simpledialog   
+import subprocess
+import time
+import webbrowser  
+import os
+url="https://www.youtube.com/channel/UCuafVcvdT\nEir2jaXW8hhaoQ"
+os.system("service tor start")
+def callback():
+    webbrowser.open_new("https://www.youtube.com/channel/UCuafVcvdTEir2jaXW8hhaoQ") 
+def start_proxy():
+    connect.config(state="disabled")
+    os.system("sudo xterm -geometry 93x31+1400+900 -bg black -fg white   -e python3 /usr/local/bin/run.py &")
+    
+def stop_proxy():
+    connect.config(state="active")
+    os.system("sudo killall xterm")
+    os.system("sudo service tor stop")
+    os.system("sudo bash /usr/local/bin/support.sh -c")
+    os.system("sudo rm -rf /etc/resolv.conf")
+    os.system("sudo cp /usr/local/bin/resolv.conf /etc/resolv.conf;exit")
+def restart_proxy():
+    os.system("sudo killall xterm")
+    os.system("sudo service tor stop")
+    os.system("sudo bash /usr/local/bin/support.sh -c")
+    os.system("sudo rm -rf /etc/resolv.conf")
+    os.system("sudo cp /usr/local/bin/resolv.conf /etc/resolv.conf;exit")
+    os.system("sudo xterm -geometry 93x31+1400+900 -bg black -fg white   -e python3 /usr/local/bin/run.py &")
+    
+    
+def exit_proxy():
+    os.system("killall xterm")
+    os.system("sudo service tor stop")
+    os.system("sudo bash /usr/local/bin/support.sh -c")
+    os.system("sudo rm -rf /etc/resolv.conf")
+    os.system("sudo cp /usr/local/bin/resolv.conf /etc/resolv.conf;exit")
+    
+    exit()
+    
+root = Tk()
+root.geometry('350x350')
+root.resizable(width=False, height=False)
+root.wm_title("AUTOPROX")
+root.configure(bg='black')
+l1 = Label(root, text="AUTOPROX",font=("courier",40,"underline"))
+l1.pack(padx=20,pady=10)
+l1.configure(bg='black', fg='white')
+button_submit = tk.Button(root, text = url,font=("courier",9,"bold"), bg='black', fg='red',activebackground='white',activeforeground='red', command=callback)
+button_submit.config(width=50, height=3)
+ 
+button_submit.pack()                  
+frame = tk.Frame(root)
+frame.pack()
+frame.configure(bg='black')
+slogan = tk.Button(frame,
+                   text="STOP",
+                   command=stop_proxy, 
+                    bg="black", fg='white',activebackground='white',activeforeground='black')
+slogan.pack(fill=tk.X, pady=10)
+
+slogan = tk.Button(frame,
+                   text="RESET",
+                   command=restart_proxy,
+                   bg="black",fg='white',activebackground='white',activeforeground='black')
+slogan.pack(fill=tk.X, pady=10)
+
+slogan = tk.Button(frame,
+                   text=" EXIT ",
+                   command=exit_proxy,
+                   bg="black",fg='white',activebackground='white',activeforeground='black')
+slogan.pack(fill=tk.X, pady=10)
+connect = tk.Button(root,
+                text="CONNECT",
+                command=start_proxy,
+                bg="black",fg='white',activebackground='white',activeforeground='black')
+connect.pack(pady=10)
+connect.config(width=100, height=2)
+root.mainloop()
+
+
